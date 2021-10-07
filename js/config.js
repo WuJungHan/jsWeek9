96df6eb2ccf4ff0056ba6b7025e285b21b2788fc
WuJungHan <eva29485577@gmail.com>
2021-10-07 13:11:31 +0800

    修改API

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-const url = 'https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer'
-const adminUrl = 'https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin'
+const url = 'https://livejs-api.hexschool.io/api/livejs/v1/customer'
+const adminUrl = 'https://livejs-api.hexschool.io/api/livejs/v1/admin'
 
 const api_path = 'eva29485577';
 const token = 'yc9dC0vcC2Ybg80j6aXBvd3ZaQn1';
